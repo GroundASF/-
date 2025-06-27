5ea4f0164bccbf0b464121ce04a27e18beb60f34
GroundASF <yichaoc489@gmail.com>
2025-06-27 19:15:31 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+// Service Worker for 五行仙途卡牌对战
+const CACHE_NAME = 'wuxing-card-game-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './game.js',
+  './manifest.json'
+];
+
+// 安装Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // 如果缓存中有，直接返回
+        if (response) {
+          return response;
+        }
+        
+        // 否则从网络获取
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 更新Service Worker
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
